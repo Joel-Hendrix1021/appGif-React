269fd2f7b30917328c1987518cb737bf3d3e54aa
Joel Pimentel <joelpimentel1995@gmail.com>
2021-08-02 09:10:30 -0500

    edit function fetchGifs

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
-import { useEffect, useState} from 'react'
+
 import './App.css';
-import Form from './components/Form';
+import FormGifs from './components/FormGifs';
+import Gifs from './components/Gifs';
+
 
 // API_KEY=fN8MEkT7XYCyvrBxosHzU5p2qaXzNJj0
 
 function App() {
-   const [gifs, setGifs] = useState([])
-
-   useEffect(() => {
-      const getApiGif=async()=>{
-        const data = await fetch('https://api.giphy.com/v1/gifs/search?api_key=8D9K3y5paFV3Ub5Hmgsl0fhHUijoLpGb&q=bleach&limit=25&offset=0&rating=g&lang=en')
-        const gif = await data.json()
-        console.log(gif)
-        saveGifs(gif.data)
-      }
-      getApiGif()
-   }, [])
-
-const saveGifs=(n)=>{
-    n.forEach(el => {
-      let data = {
-        id: el.id,
-        url: el.images.downsized_medium.url
-      }
-      setGifs(gif=> [...gif, data])
-    });
-}
-
-
-
+ 
   return (
     <div className="App">
-       <h1>Hola</h1>
-       <Form/>
-        {
-         gifs.map(gif=>{
-           return(
-            <div key={gif.id}>
-               <img  src={gif.url} alt="" />
-            </div>
-            
-           )
-         })
-       } 
+       <h1>Gifi<span id="title">App</span></h1>
+       <FormGifs />
+       <Gifs keyword='panda'/>
     </div>
   );
 }
